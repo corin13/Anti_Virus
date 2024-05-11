9c60f23a55ea6b8de24adc5d932ef47c25cab4a7
ysbeam <ysbeam@naver.com>
2024-05-11 23:49:09 +0000

    test again

--- test.cpp
+++ test.cpp
@@ -3,10 +3,7 @@
 int main(void){
     printf("test");
     // 김초은 입니다~~
-<<<<<<< HEAD
-    // 양소영 입니다~~
-=======
     // 최빈 입니다~~
->>>>>>> origin/master
+    // 양소영 입니다~~ + 한 번 더 해보기
     return 0;
 }
