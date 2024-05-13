e75944c0fc0de61042814293c1947b2d10d60fd4
hun <chamgf5247@naver.com>
2024-05-13 16:43:39 +0900

    main_ex1

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+// void CheckOpt(int option){
+
+// }
+
+
+int main(int argc, char* argv[]){
+    if (argc > 1) 
+        for (int i=0; i < argc; i++)
+            cout << argv[i] << endl;
+
+    else
+        cout << "Try 'UdkdAgent --help' for more information." << endl;
+
+    int option;
+    while((option = getopt(argc, argv, "bfs") != -1)){
+    }
+
+
+    return 0;
+}
--- test.cpp
+++
@@ -1,8 +0,0 @@
-#include "test.h"
-
-int main(){
-
-    print();
-    // 차훈영입니다~~
-    return 0;
-}
