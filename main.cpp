f9d1aee06063d41ea8fb7c697dfaaaf75d81405c
hun <chamgf5247@naver.com>
2024-05-13 20:53:21 +0900

    ex1

--- main.cpp
+++ main.cpp
@@ -1,25 +1,53 @@
 #include <iostream>
-#include <unistd.h>
+#include <getopt.h>
+
 
 using namespace std;
 
-// void CheckOpt(int option){
+struct option options[]={
+    {"help", no_argument, 0, 'h'},
+    {"info", no_argument, 0, 'i'},
+    {"background",no_argument, 0, 'b'},
+    {"scan", no_argument, 0,'s'}, //인자값 필요로 한다면 no_argument -> required_argument
+    {0,0,0,0}
+};
 
-// }
+void CheckOpt(int argc, char** argv){
+    int optionIndex= 0;
+    int opt;
+    const char* option="hibs"; //인자값 필요로 한다면 :붙이기 ex) hib:s:
 
+    while((opt = getopt_long(argc, argv, option, options, &optionIndex)) != -1 ){
+        switch(opt){
+            case 'h':
+                cout << "HELP" << endl; //기능 함수 넣기
+                break;
 
-int main(int argc, char* argv[]){
+            case 'i':
+                cout << "INFO" << endl;
+                break;
+
+            case 'b':
+                cout << "BACKGROUND" << endl;
+
+            case '?':
+                cout << "Error" << endl;
+                break;
+
+            default:
+                abort();
+        }
+    }
+
+}
+
+int main(int argc, char **argv){
+    
+    //옵션 값 확인
     if (argc > 1) 
-        for (int i=0; i < argc; i++)
-            cout << argv[i] << endl;
-
+        CheckOpt(argc, argv);
     else
         cout << "Try 'UdkdAgent --help' for more information." << endl;
-
-    int option;
-
-    while((option = getopt(argc, argv, "bfs:") != -1)){
-    }
 
 
     return 0;
