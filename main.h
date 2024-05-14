0746d22ea5f2928395467b6f0a48fde916fd87d7
hun <chamgf5247@naver.com>
2024-05-14 18:38:04 +0900

    test

--- main.h
+++ main.h
@@ -1,5 +1,6 @@
 #include <getopt.h>
 #include <iostream>
+#include <thread>
 
 using namespace std;
 //pr test test2
@@ -15,9 +16,29 @@ void info(){
     cout << "이 프로그램은 .. " <<endl;
 }
 
-void background(){
-    cout << "이 프로그램은 .. " <<endl;
+int logic1(){
+
+
+    return 0;
 }
+
+int logic2(){
+    return 0;
+}
+
+void background(){
+    while (1){
+        cout << "Anti-debugging Logic Running…" << endl;
+
+        logic1();
+        logic2();
+        this_thread::sleep_for(chrono::seconds(1));
+        
+    }
+}
+
+
+
 
 void scan(){
     cout << "이 프로그램은 .. " <<endl;
