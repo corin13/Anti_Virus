bdabeec2b2aca81baa552efaa4093ed04e26bb4c
chlqls <chlqls212@gmail.com>
2024-05-15 20:28:49 +0900

    #31 옵션 예외처리 및 주석 추가

--- scan.cpp
+++ scan.cpp
@@ -11,6 +11,7 @@
 
 using namespace std;
 
+//-s 혹은 --scan 옵션 입력 시 scan() 함수 실행됨
 void scan(){
     cout << "Please enter the path : ";
     string path;
@@ -26,9 +27,14 @@ void scan(){
     cin >> option;
     cin.ignore();
 
+    if (option != 1 && option != 2) {
+        cerr << "Invalid option selected. Please enter 1 or 2.\n";
+        return; // 잘못된 입력일 경우 함수 종료
+    }
+
     cout << "\n### File Scan Start ! (Path : " << path << " , Option : " << option << ") ###\n\n";
 
-    // 파일 순회
+    // 사용자가 지정한 디렉토리 내의 모든 파일을 순회하면서 악석파일 검사
     scanDirectory(path, option);
 
     return;
@@ -39,17 +45,18 @@ void scanDirectory(const string& path, int option) {
     char * const paths[] = {const_cast<char *>(path.c_str()), nullptr};
 
     FTS *file_system = fts_open(paths, FTS_NOCHDIR | FTS_PHYSICAL, nullptr);
-    if (!file_system) {
-        cerr << "\nFailed to open the directory path.\n";
+    if (file_system == NULL) {
+        cerr << "\n\033[31mFailed to open the directory path.\033[0m\n";
         return;
     }
 
     FTSENT *node;
     int file_count = 0;
     long long total_size = 0;
-    vector<string> detectedMalware;
-    vector<string> hashes = loadHashes("hashes.txt");
+    vector<string> detectedMalware; // 악성파일로 판별된 파일의 경로를 저장
+    vector<string> hashes = loadHashes("hashes.txt"); // hashes.txt는 악성파일 해시값이 저장되어있는 텍스트 파일
 
+    // 파일을 한개씩 순회해서 사용자가 입력한 옵션에 따라 검사
     while ((node = fts_read(file_system)) != nullptr) {
         if (node->fts_info == FTS_F) {
             file_count++;
@@ -64,7 +71,7 @@ void scanDirectory(const string& path, int option) {
     }
 
     if (fts_close(file_system) < 0) {
-        cerr << "\nFailed to close the file system.\n";
+        cerr << "\n\033[31mFailed to close the file system.\033[0m\n";
     }
 
     cout << "\n### End File Scan ###\n\n";
@@ -79,16 +86,17 @@ void scanDirectory(const string& path, int option) {
 }
 
 void compareByHash(FTSENT *node, vector<string>& detectedMalware, vector<string>& hashes) {
-    string fileHash = computeSHA256(node->fts_path);
-        for (const auto& hash : hashes) {
-            if (fileHash == hash) {
-                detectedMalware.push_back(node->fts_path);
-                cout << "\n\033[31m[+] Malware detected: [" << node->fts_path << "]\033[0m\n\n";
-                break;
-            }
+    string fileHash = computeSHA256(node->fts_path); // 파일의 해시값을 계산
+    for (const auto& hash : hashes) {
+        if (fileHash == hash) { //계산된 해시값을 저장된 해시값들과 비교
+            detectedMalware.push_back(node->fts_path);
+            cout << "\n\033[31m[+] Malware detected: [" << node->fts_path << "]\033[0m\n\n";
+            break;
         }
+    }
 }
 
+// hashes.txt의 내용을 vector로 변환
 vector<string> loadHashes(const string& filename) {
     ifstream file(filename);
     vector<string> hashes;
@@ -100,10 +108,11 @@ vector<string> loadHashes(const string& filename) {
     return hashes;
 }
 
+// SHA256 해시알고리즘을 사용해서 파일의 해시값을 계산
 string computeSHA256(const string& filename) {
     ifstream file(filename, ifstream::binary);
     if (!file) {
-        cerr << "Cannot open file!\n";
+        cerr << "\n\033[31mCannot open file!\033[0m\n";
         return "";
     }
 
