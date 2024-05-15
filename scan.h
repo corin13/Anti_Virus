7c094c8892cfb8e07b7a682c6f18ba5520812aab
hun <chamgf5247@naver.com>
2024-05-15 22:28:46 +0900

    add antidbg logic1

--- scan.h
+++ scan.h
@@ -2,10 +2,16 @@
 #define SCAN_H
 
 #include <string>
+#include <vector>
+#include <fts.h>
+
+using namespace std;
 
 void scan();
-void scanDirectory(const std::string& path, int option);
-void compareByHash();
+void scanDirectory(const string& path, int option);
+void compareByHash(FTSENT *node, vector<string>& detectedMalware, vector<string>& hashes);
+vector<string> loadHashes(const string& filename);
+string computeSHA256(const string& filename);
 void checkYaraRule();
 
 #endif
