2ada2fb8d1ec630d763dee1de5bc1a12195bc3f6
chlqls <chlqls212@gmail.com>
2024-05-15 19:26:01 +0900

    #31 -s (scan) 옵션: hash값 비교 기능 구현

--- scan.cpp
+++ scan.cpp
@@ -3,13 +3,15 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fts.h>
+#include <fstream>
+#include <sstream>
+#include <openssl/sha.h>
+#include <iomanip>
 #include "scan.h"
 
 using namespace std;
 
 void scan(){
-//    cout << "이 프로그램은 .. scan " << endl;
-
     cout << "Please enter the path : ";
     string path;
     getline(cin, path);
@@ -46,6 +48,7 @@ void scanDirectory(const string& path, int option) {
     int file_count = 0;
     long long total_size = 0;
     vector<string> detectedMalware;
+    vector<string> hashes = loadHashes("hashes.txt");
 
     while ((node = fts_read(file_system)) != nullptr) {
         if (node->fts_info == FTS_F) {
@@ -56,7 +59,7 @@ void scanDirectory(const string& path, int option) {
         if(option == 1) {
             checkYaraRule();
         }else if(option == 2) {
-            compareByHash();
+            compareByHash(node, detectedMalware, hashes);
         }
     }
 
@@ -68,16 +71,60 @@ void scanDirectory(const string& path, int option) {
 
     // 스캔 결과 출력
     cout << "\n- File Scan Result -\n\n"
-            << "[+] Total Malware File : " << detectedMalware.size() << " files\n";
+            << "\033[31m[+] Total Malware File : " << detectedMalware.size() << " files\033[0m\n";
     for (int i = 0; i < detectedMalware.size(); ++i) {
-        cout << "[" << i + 1 << "] : " << detectedMalware[i] << "\n";
+        cout << "\033[31m[" << i + 1 << "] : " << detectedMalware[i] << "\033[0m\n";
     }
     cout << "\n[+] Total Scan File : " << file_count << " files " << total_size << " bytes\n";
 }
 
-void compareByHash() {
-
+void compareByHash(FTSENT *node, vector<string>& detectedMalware, vector<string>& hashes) {
+    string fileHash = computeSHA256(node->fts_path);
+        for (const auto& hash : hashes) {
+            if (fileHash == hash) {
+                detectedMalware.push_back(node->fts_path);
+                cout << "\n\033[31m[+] Malware detected: [" << node->fts_path << "]\033[0m\n\n";
+                break;
+            }
+        }
 }
+
+vector<string> loadHashes(const string& filename) {
+    ifstream file(filename);
+    vector<string> hashes;
+    string line;
+    while (getline(file, line)) {
+        hashes.push_back(line);
+    }
+    file.close();
+    return hashes;
+}
+
+string computeSHA256(const string& filename) {
+    ifstream file(filename, ifstream::binary);
+    if (!file) {
+        cerr << "Cannot open file!\n";
+        return "";
+    }
+
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    char buffer[1024];
+    while (file.read(buffer, sizeof(buffer))) {
+        SHA256_Update(&sha256, buffer, file.gcount());
+    }
+    SHA256_Update(&sha256, buffer, file.gcount());
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_Final(hash, &sha256);
+
+    stringstream ss;
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        ss << hex << setw(2) << setfill('0') << (int)hash[i];
+    }
+
+    return ss.str();
+}
+
 
 void checkYaraRule() {
 
