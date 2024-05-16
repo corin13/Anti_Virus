618bff0575df3d52fb9892f61e5a92884c0408c4
chlqls <chlqls212@gmail.com>
2024-05-17 00:32:28 +0900

    -s 옵션 경로 유효성 검사 추가

--- scan.h
+++ scan.h
@@ -8,10 +8,12 @@
 using namespace std;
 
 void scan();
+bool isDirectory(const string& path);
 void scanDirectory(const string& path, int option);
 void compareByHash(FTSENT *node, vector<string>& detectedMalware, vector<string>& hashes);
 vector<string> loadHashes(const string& filename);
 string computeSHA256(const string& filename);
+void printError(const string& message);
 void checkYaraRule();
 
 #endif
