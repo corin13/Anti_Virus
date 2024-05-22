b828a7bbb198088a50e812e18b2502a54cd5e90d
ysbeam <ysbeam@naver.com>
2024-05-22 16:11:29 +0000

    feat : 비동기 로깅 시스템 설정 #80

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 void CheckOption(int &argc, char** &argv){
     int optionIndex= 0;
     int opt;
-    const char* option="hids"; //인자값 필요로 한다면 :붙이기 ex) hib:s:
+    const char* option="hidpsl"; //인자값 필요로 한다면 :붙이기 ex) hib:s:
 
     while((opt = getopt_long(argc, argv, option, options, &optionIndex)) != -1 ){
         switch(opt){
