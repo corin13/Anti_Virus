1a4832bd53fdb7e0f72c09cae3d4807b505b08fe
ysbeam <ysbeam@naver.com>
2024-06-16 06:46:51 +0000

    feat : -u 옵션 사용자에게 단계별로  입력받도록 수정 #142

--- usage_collector.h
+++ usage_collector.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include "ansi_color.h"
 #include "error_codes.h"
 
 #define BUFFER_SIZE 128
@@ -12,6 +13,7 @@ public:
     int GetCpuUsage(std::string& strResult);
     int GetDiskUsage(std::string& strResult);
     int GetNetworkUsage(std::string& strResult);
-    int SaveUsageToFile(const std::string& strFileName);
+    int SaveUsageToFile(const std::string& strFileName, bool cpu, bool disk, bool network);
     int CollectAndSaveUsage();
+    int GetNetworkInterfaces(std::vector<std::string>& interfaces);
 };
